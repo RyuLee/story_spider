47be1cc9a498a086425ade032f8bc648bc05b205
ryulee2014@gmail.com <hupochuan0916>
2016-01-05 13:10:19 +0800

    init the crawler

---
+++ crawler.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 04 18:42:59 2016
+
+@author: v-lipe
+"""
+import scrapy
+
+class Spider(scrapy.spider):
+    def __init__():
+        return
+    name = "spider"
+    start_urls = ['http://www.moralstories.org/']
+    
